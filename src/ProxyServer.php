02a0c6157f89550da0256a910623e31e2e71b6f5
xRookieFight <me@xrookiefight.com>
2026-01-30 12:54:56 +0300

    Next: 1.0.0-alpha3

--- src/ProxyServer.php
+++ src/ProxyServer.php
@@ -64,7 +64,7 @@ use const DIRECTORY_SEPARATOR;
 class ProxyServer
 {
 	public const NAME = 'AquaRelay';
-	public const VERSION = '1.0.0-alpha2'; // Semver
+	public const VERSION = '1.0.0-alpha3'; // Semver
 	public const IS_DEVELOPMENT = true;
 	public RakLibInterface $interface;
 	private MainLogger $logger;
